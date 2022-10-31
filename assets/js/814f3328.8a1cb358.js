bdee69fa9f63276f131d19b777b9afec3c2af72b
Sivabharathy <gotocva@gmail.com>
2022-10-31 18:17:40 +0530

    design update

--- assets/js/814f3328.8a1cb358.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgotocva_blog=self.webpackChunkgotocva_blog||[]).push([[2535],{5641:t=>{t.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Basics of computers","permalink":"/blog/basics-of-computers"},{"title":"HTTP Status codes","permalink":"/blog/http-status-codes"},{"title":"Data structures in JS","permalink":"/blog/js-data-structures"}]}')}}]);
